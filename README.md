32e40521a8392728582af23b11516486346aa4b0
Pankaj Gupta <pgupta584@gmail.com>
2025-01-12 17:03:50 +0530

    Selenium Grid in Hum Mode using Container

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Java-Selenium
 Java Selenium Testing with Docker
+
+# Repo Link
+https://github.com/pgupta584/Java-Selenium
+
+# Clone Repo
+git clone https://github.com/pgupta584/Java-Selenium.git
+
+# Run Using Maven
+mvn clean test
--- src/test/java/Utils/BaseTest.java
+++ src/test/java/Utils/BaseTest.java
@@ -1,22 +1,62 @@
 package Utils;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.sun.jdi.connect.spi.TransportService;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
-        // Initialize the WebDriver
-        driver = new SafariDriver();
+    public void setUp() throws MalformedURLException {
+        // get the System Property
+        String browser = System.getProperty("browser");
+        String grid_enable_status = System.getProperty("selenium.grid.enabled");
+        System.out.println("Browser is "+browser+ " grid_enable_status is "+grid_enable_status);
+        Capabilities capabilities;
+        if(browser.equalsIgnoreCase("chrome")){
+            capabilities = new ChromeOptions();
+        }else{
+            capabilities = new FirefoxOptions();
+        }
+        if(grid_enable_status.equalsIgnoreCase("true")){
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+        }else{
+            driver = new ChromeDriver();
+        }
         driver.get("https://www.saucedemo.com"); // Replace with your practice website URL
     }
+
+//    @BeforeMethod
+//    public void setUp() throws MalformedURLException {
+//        // Initialize the WebDriver
+////        driver = new SafariDriver();
+////        driver.manage().window().maximize();
+////        driver.get("https://www.saucedemo.com"); // Replace with your practice website URL
+//
+////        System.setProperty("selenium.trace.openTelemetry", "false");
+////        System.setProperty("selenium.trace", "false");
+////        Capabilities capabilities = new ChromeOptions();
+//        Capabilities capabilities = new FirefoxOptions();
+//        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+//
+//
+////        driver.manage().window().maximize();
+//        driver.get("https://www.saucedemo.com"); // Replace with your practice website URL
+//    }
+
 
     @AfterMethod
     public void tearDown() {
